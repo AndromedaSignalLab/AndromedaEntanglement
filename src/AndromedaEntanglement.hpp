53078d96f06fef56e25c52b152049dc333ddb8ae
Volkan Orhan <contactme@volkanorhan.com>
2026-06-10 20:30:40 +0300

    Source code descriptions are updated

--- src/AndromedaEntanglement.hpp
+++ src/AndromedaEntanglement.hpp
@@ -1,5 +1,5 @@
 /*
-AndromedaEntanglement class declarations and definitions of Andromeda Entanglement Project
+AndromedaEntanglement class declarations of Andromeda Entanglement Project
 Copyright (C) 2026 Volkan Orhan
 
 This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
--- test/AndromedaEntanglementTest.cpp
+++ test/AndromedaEntanglementTest.cpp
@@ -1,6 +1,6 @@
 /*
-FrequencyBandTest test function definitions of DSP Project
-Copyright (C) 2020 Volkan Orhan
+Test function definitions of Andromeda Entangclement Project
+Copyright (C) 2026 Volkan Orhan
 
 This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
 
